dc4eb7afc261828a017f3a192fb722e78de93a5b
Sidhartgautam <developer.siddharthagautam@gmail.com>
2025-02-05 15:16:26 +0545

    static changed

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,4 @@
-# Create necessary directories
-mkdir -p static 
-# Install dependencies
+mkdir -p static
 pip3 install -r requirements.txt
-
-# Collect static files
 python3 manage.py collectstatic --noinput
-
-# Create migrations (optional, if needed)
 python3 manage.py migrate
--- core/settings.py
+++ core/settings.py
@@ -200,9 +200,8 @@ CLOUDINARY_STORAGE = {
 #     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 
