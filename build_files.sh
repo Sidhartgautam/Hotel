d60a63c24cbe528096cbafaa1513768a4390c92c
Sidhartgautam <developer.siddharthagautam@gmail.com>
2025-03-01 10:23:32 +0545

    static added

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-mkdir -p staticfiles_build
-export STATIC_ROOT=staticfiles_build
+mkdir -p static
+export STATIC_ROOT=static
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
