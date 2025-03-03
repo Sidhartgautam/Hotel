e71614bf37a8a21a97e8f9369a87d0a478c367a5
Sidhartgautam <developer.siddharthagautam@gmail.com>
2025-03-03 13:09:42 +0545

    optimized of country-code

--- offers/views.py
+++ offers/views.py
@@ -8,42 +8,39 @@ from core.utils.response import PrepareResponse
 
 class WeeklyOffersView(APIView):
     def get(self, request):
-        # Extract 'from' and 'to' dates from query parameters
         from_date = request.query_params.get('from')
         to_date = request.query_params.get('to')
+        country_code = request.country_code 
+        if not country_code:
+            return Response({
+                "success": False,
+                "message": "Country code is required.",
+                "data": []
+            }, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            # Parse the dates if provided
             from_date = date.fromisoformat(from_date) if from_date else None
             to_date = date.fromisoformat(to_date) if to_date else None
         except ValueError:
             return Response({
                 "success": False,
                 "message": "Invalid date format. Use 'YYYY-MM-DD'.",
-                "data": {}
+                "data": []
             }, status=status.HTTP_400_BAD_REQUEST)
-
-        # Ensure 'from' date is earlier than or equal to 'to' date
         if from_date and to_date and from_date > to_date:
             return Response({
                 "success": False,
                 "message": "'from' date must be earlier than or equal to 'to' date.",
-                "data": {}
+                "data": []
             }, status=status.HTTP_400_BAD_REQUEST)
-
-        # Filter offers based on date range or fetch all offers if no dates are provided
+        weekly_offers = WeeklyOffer.objects.filter(property__country__country_code=country_code)
         if from_date and to_date:
-            weekly_offers = WeeklyOffer.objects.filter(
-                start_date__lte=to_date,  # Offer starts on or before the 'to' date
-                end_date__gte=from_date  # Offer ends on or after the 'from' date
+            weekly_offers = weekly_offers.filter(
+                start_date__lte=to_date, 
+                end_date__gte=from_date  
             )
-        else:
-            weekly_offers = WeeklyOffer.objects.all()
-
-        # Serialize the filtered offers
         serializer = WeeklyOfferSerializer(weekly_offers, many=True)
 
-        # Return the response
         return Response({
             "success": True,
             "message": "Weekly offers retrieved successfully",
